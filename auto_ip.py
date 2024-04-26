7cce205b0fd7ff4bd689216ec9c136155498d9a2
Lairlynx <vladut.lazar@student.unitbv.ro>
2024-04-26 13:38:29 +0300

    ACTUALLY WORKS

--- auto_ip.py
+++ auto_ip.py
@@ -7,9 +7,9 @@ df = pd.read_excel(r'\\vt1.vitesco.com\SMT\didt1002\05_IT_MES\IP_MES.xlsx', shee
 
 # Iterate over the rows of the DataFrame
 for index, row in df.iterrows():
-    pc_name = row.get('B', 'Default Value')
-    pc_ip = row.get('C', 'Default Value')
-    pc_order = row.get('A', 'Default Value')
+    pc_name = row.get('Nume Linie', 'Default Value')
+    pc_ip = row.get('IP', 'Default Value')
+    pc_order = row.get('NR Crt', 'Default Value')
 
     # Defining the target path and the shortcut file
     target_path = f"\\\\{pc_order}"
@@ -19,7 +19,7 @@ for index, row in df.iterrows():
     shortcut_folder = r'C:\Users\uiv55706\Desktop\PC_Prod'
 
     # Using subprocess to run a command that creates the shortcut
-    command = f'powershell "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(\'{os.path.join(shortcut_folder, shortcut_file)}\'); $s.Target_path = \'{target_path}\'; $s.Save()"'
+    command = f'powershell "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(\'{os.path.join(shortcut_folder, shortcut_file)}\'); $s.TargetPath = \'{target_path}\'; $s.Save()"'
     subprocess.run(command, shell=True)
 
 print("Shortcuts created successfully!")
