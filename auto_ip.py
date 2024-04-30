f7397ac823f85304bcc08613ea6066ec8c25363c
Lairlynx <vladut.lazar@student.unitbv.ro>
2024-04-30 12:05:43 +0300

    1.0.1 Version

--- auto_ip.py
+++ auto_ip.py
@@ -10,7 +10,7 @@ def sanitize_pc_name(pc_name):
     return sanitized_name.strip()  # Remove leading/trailing spaces
 
 # Loading the spreadsheet and limiting the range to avoid reading beyond line 155
-excel_path = r'C:\Users\uiv55706\Desktop\IP.xlsx'
+excel_path = r'\\vt1.vitesco.com\SMT\didt1002\05_IT_MES\IP_MES.xlsx'
 df = pd.read_excel(excel_path, sheet_name="IP", nrows=155)  # Read only the first 155 rows
 
 # Iterate over the rows of the DataFrame
@@ -20,7 +20,7 @@ for index, row in df.iterrows():
     pc_ip = row.get('IP ', 'Default Value')
 
     # Defining the target path and the shortcut file
-    target_path = f"\\\\{pc_ip}\d$"  # Use UNC path if applicable
+    target_path = f"\\\\{pc_ip}\d$"
     shortcut_file = f"{pc_name_sanitized}.lnk"  # Use sanitized name
 
     # Skip the row if it contains empty or invalid data
@@ -28,14 +28,18 @@ for index, row in df.iterrows():
         continue
 
     # Defining the location where the shortcuts are created
-    shortcut_folder = r'C:\Users\uiv55706\Desktop\PC_Prod'
+    shortcut_folder = r'\\vt1.vitesco.com\SMT\didt1083\01_MES_PUBLIC\PC_Prod'
 
-    try:
-        # Using subprocess to run a command that creates the shortcut
-        command = f'powershell "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(\'{os.path.join(shortcut_folder, shortcut_file)}\'); $s.TargetPath = \'{target_path}\'; $s.Description = \'{pc_ip}\'; $s.Save()"'
-        subprocess.run(command, shell=True)
-        print(f"Shortcut for {pc_name} created successfully!")
-    except Exception as e:
-        print(f"Error creating shortcut for {pc_name}: {str(e)}")
+     # Check if the shortcut file already exists
+    if not os.path.exists(os.path.join(shortcut_folder, shortcut_file)):
+        try:
+            # Using subprocess to run a command that creates the shortcut
+            command = f'powershell "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(\'{os.path.join(shortcut_folder, shortcut_file)}\'); $s.TargetPath = \'{target_path}\'; $s.Description = \'{pc_ip}\'; $s.Save()"'
+            subprocess.run(command, shell=True)
+            print(f"Shortcut for {pc_name} created successfully!")
+        except Exception as e:
+            print(f"Error creating shortcut for {pc_name}: {str(e)}")
+    else:
+        print(f"Shortcut for {pc_name} already exists. Skipping.")
 
 print("All shortcuts processed.")
