9cc5e60aab14eb2de34ca18d40e0f887527f2d57
Lairlynx <vladut.lazar@student.unitbv.ro>
2024-04-30 09:33:27 +0300

    Removes Invalid Characters

--- auto_ip.py
+++ auto_ip.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import os
 import subprocess
+import re
 
 # Loading the spreadsheet
-df = pd.read_excel(r'\\vt1.vitesco.com\SMT\didt1002\05_IT_MES\IP_MES.xlsx', sheet_name="IP")
+df = pd.read_excel(r'C:\Users\uiv55706\Desktop\IP.xlsx', sheet_name="IP")
+
+def sanitize_pc_name(pc_name):
+    # Replace invalid characters with underscores
+    sanitized_name = re.sub(r'[^\\w_. -]', '_', pc_name)
+    return sanitized_name
 
 # Iterate over the rows of the DataFrame
 for index, row in df.iterrows():
     pc_name = row.get('Nume Linie', 'Default Value')
     pc_ip = row.get('IP', 'Default Value')
     pc_order = row.get('NR Crt', 'Default Value')
+
+    pc_name_sanitized = sanitize_pc_name(pc_name)  # Apply sanitization
 
     # Defining the target path and the shortcut file
     target_path = f"\\\\{pc_order}"
